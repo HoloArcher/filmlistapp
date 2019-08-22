33a4bf47b6b80c0625c25c04a4fd3459c66f4853
Schiess Josiah <in17scjo@tfbern.ch>
2019-08-22 16:50:42 +0200

    .

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-container grid-list-xs>
+      <v-layout justify-center row wrap>
+        <v-flex>
+          <!-- <NewEntry /> -->
+          <v-btn dark disabled>text</v-btn>
+          <app-table v-bind:headers="headers" v-bind:items="items" />
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NewEntry from "./components/NewEntry";
+import Table from "./components/Table";
+import axios from 'axios'
+import Vue from 'vue'
 
 export default {
-  name: 'app',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
-</script>
+    NewEntry,
+    "app-table": Table
+  },
+  async mounted() {
+    // this.items = [{ name: "josiah", value: "kek", description: 'why are we still here? Just to suffer? Everyday i can feel my leg, my arm, my fingers. where they used to be' }];
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    var obj = await axios.get('/api')
+   
+
+    
+    Vue.set(this,'items',obj.data.items)
+    Vue.set(this,'headers',obj.data.headers)
+    
+  },
+  
+  data: () => ({
+    kek: '',
+    items: [],
+    headers: []
+  })
+};
+</script>
---
+++ src/components/Table.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-container grid-list-xs >
+    <v-app-bar app color="primary" >
+      <v-spacer></v-spacer>
+      <v-flex xs6 offset-xs-2 style="margin: auto">
+        <v-text-field 
+        v-model="search" 
+        solo
+        clearable 
+        placeholder="Search" 
+        >
+        </v-text-field>
+      </v-flex>
+      <v-spacer></v-spacer>
+     
+    </v-app-bar>
+    <v-divider></v-divider>
+    <v-data-table
+      :items-per-page="100"
+      item-key="name"
+      show-expand
+      single-expand
+      :expanded.sync="expanded"
+      :search="search"
+      :headers="headers"
+      :items="items"
+      class="elevation-1"
+    >
+      <template v-slot:expanded-item="{headers}">
+        <td :colspan="headers.length">
+          <v-container grid-list-xs>
+            <v-layout row wrap>
+              <v-flex xs12 v-if="expanded.length > 0 ">{{ expanded[0].name }}</v-flex>
+            </v-layout>
+          </v-container>
+        </td>
+      </template>
+    </v-data-table>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: ["items", "headers"],
+  data() {
+    return {
+      btn: false,
+      search: "",
+      expanded: []
+    };
+  }
+};
+</script>
+
+<style>
+</style>
